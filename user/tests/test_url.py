0ab0281dc12aa58fc08a309e90894768b420ffa9
emadam <ldy9037@naver.com>
2022-07-08 08:55:21 +0900

    test: user create test에 phone_number와 password 추가

--- user/tests/test_url.py
+++ user/tests/test_url.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from argon2 import PasswordHasher
 from rest_framework import status
 from rest_framework.test import APITestCase
 from user.models import User
@@ -7,8 +8,19 @@ class UserTests(APITestCase):
     def test_create_user(self):
         
         url = reverse('list-insert-users')
-        data = {'email': 'ldy9037@naver.com', 'name': '이동열', 'nickname': 'hani_6_6'}
+        data = {
+            'email': 'ldy9037@naver.com', 
+            'name': '이동열', 
+            'nickname': 'hani_6_6',
+            'phone_number': '010-5264-5565',
+            'password': PasswordHasher().hash("12345678")
+         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().email, 'ldy9037@naver.com')
         self.assertEqual(User.objects.get().name, '이동열')
+        self.assertEqual(User.objects.get().nickname, 'hani_6_6')
+        self.assertEqual(User.objects.get().phone_number, '010-5264-5565')
+        self.assertTrue(PasswordHasher().verify(User.objects.get().password, "12345678"))
+        
