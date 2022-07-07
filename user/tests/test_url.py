f5b915146b61b2fb6297c900084e89e257e5789e
emadam <ldy9037@naver.com>
2022-07-07 20:51:24 +0900

    test: user create에 대한 test case 작성

---
+++ user/tests/test_url.py
@@ -0,0 +1,14 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from user.models import User
+
+class UserTests(APITestCase):
+    def test_create_user(self):
+        
+        url = reverse('list-insert-users')
+        data = {'email': 'ldy9037@naver.com', 'name': '이동열'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().name, '이동열')
