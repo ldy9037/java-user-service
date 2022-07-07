cbee3972821396071d92779be1864a4351d22e32
emadam <ldy9037@naver.com>
2022-07-08 08:53:01 +0900

    feat: serializer에 nickname, phone_number, password 추가

--- user/serializers.py
+++ user/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
-        fields = ('id', 'email', 'name')
+        fields = ('id', 'email', 'name', 'nickname', 'phone_number', 'password')
