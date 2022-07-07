6421f4809747b9d49df6983b6b08987888b16175
emadam <ldy9037@naver.com>
2022-07-08 08:31:07 +0900

    feat: 휴대폰 번호에 대한 커스텀 validator 추가

--- user/validators.py
+++ user/validators.py
@@ -10,3 +10,12 @@ def validate_name(value):
             _('%(value)는 이름 형식이 아닙니다.'),
             params={'value': value},
         )
+
+def validate_phone_number(value):
+    pattern = re.compile(r'^01([0-9])-([0-9]{3,4})-([0-9]{4})$')
+
+    if not pattern.match(str(value)):
+        raise ValidationError(
+            _('%(value)는 휴대폰 번호 형식이 아닙니다.'),
+            params={'value': value},
+        )
