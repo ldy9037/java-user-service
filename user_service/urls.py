99241243c134d7423bb8ea84d97c314e640f038d
emadam <ldy9037@naver.com>
2022-07-07 20:37:50 +0900

    refactor: list_insert_users에 name 지정

--- user_service/urls.py
+++ user_service/urls.py
@@ -5,5 +5,5 @@ from user import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', views.list_insert_users, name='index'),
+    path('user/', views.list_insert_users, name='list_insert_users'),
 ]
