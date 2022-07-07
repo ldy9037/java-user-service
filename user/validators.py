cbd5aebff0f9897c2ebc0c3e95fd8bcf0d914fff
emadam <ldy9037@naver.com>
2022-07-07 18:51:13 +0900

    fix: 잘못 작성된 정규 표현식 수정 기존 정규 표현식은 영문자(대/소)나 한글이 하나라도 포함되어 있는지를 확인하던 표현식이였음 전체가 영문자 또는 한글로만 이루어져 있는지 확인하는 정규표현식으로 변경

--- user/validators.py
+++ user/validators.py
@@ -3,9 +3,9 @@ from django.utils.translation import gettext_lazy as _
 import re
 
 def validate_name(value):
-    pattern = re.compile("[a-zA-Z가-힣]")
+    pattern = re.compile(r'^[a-zA-Z가-힣]*$')
 
-    if pattern.match(str(value)):
+    if not pattern.match(str(value)):
         raise ValidationError(
             _('%(value)는 이름 형식이 아닙니다.'),
             params={'value': value},
