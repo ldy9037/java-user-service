beac0a8dda86bb09ff75b0f55e8f7f5a296be346
emadam <ldy9037@naver.com>
2022-07-08 08:54:04 +0900

    test: validator 테스트 부분 제거 후 phone_number, password test 추가

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -1,22 +1,22 @@
-from django.core.exceptions import ValidationError
+from ast import Pass
 from django.test import TestCase
-import re
-
 from user.models import User
-
+from argon2 import PasswordHasher
 
 class UserTest(TestCase):
 
     def test_user_validation(self):
-        user = User( email = "ldy9037@naver.com", name = "이동열", nickname="hani_6_6")
+        user = User( 
+            email = "ldy9037@naver.com", 
+            name = "이동열", 
+            nickname="hani_6_6", 
+            phone_number="010-5264-5565",
+            password = PasswordHasher().hash("12345678")
+        )
         self.assertEqual(user.email, "ldy9037@naver.com")
         self.assertEqual(user.name, "이동열")
         self.assertEqual(user.nickname, "hani_6_6")
+        self.assertEqual(user.phone_number, '010-5264-5565')
+        self.assertTrue(PasswordHasher().verify(user.password, "12345678"))
         user.full_clean()
-
-        with self.assertRaises(ValidationError):
-            user.name = "이동열!"
-            user.full_clean()
-
-            
             
