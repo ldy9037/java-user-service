877ed679926326a0caf617e0db417b3614b177b1
emadam <ldy9037@naver.com>
2022-07-07 18:53:11 +0900

    feat: user model에 사용자 email과 name 추가 name에는 커스텀 validator인 validate_name 적용

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from .validators import validate_name
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(
+        max_length=254, 
+        unique=True, 
+        null=False
+        )
+
+    name = models.CharField(
+        max_length=30, 
+        null=False,
+        validators=[validate_name]
+    )
