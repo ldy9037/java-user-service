1be05549308cf5a5654186057af5bb6b70b356a7
emadam <ldy9037@naver.com>
2022-07-08 21:19:03 +0900

    feat: 회원 정보 생성 View 추가 - Get 메서드 제거 - View 이름에서 list 제거 - 인증 정보 확인 로직 추가 - 비밀번호 암호화 로직 추가

--- user/views.py
+++ user/views.py
@@ -3,19 +3,26 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
 from .models import User
+from certification.models import Certification
+from argon2 import PasswordHasher
+from user_service.validators import validate_password
 
-@api_view(['GET','POST'])
-def list_insert_users(request):
-    if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-    elif request.method == 'POST':
+@api_view(['POST'])
+def insert_users(request):
+    if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-        
-    return Response(serializer.data)
+        if serializer.is_valid() and request.data['cert_id'] and request.data['plain_password']:
+            certification = Certification.objects.filter(id=request.data['cert_id'], phone_number=request.data['phone_number'], certified=1)
+            validate_password(request.data['plain_password'])
+
+            if certification.count():
+                password = PasswordHasher().hash(request.data['plain_password'])
+                serializer.save()
+                User.objects.filter(id=serializer.data["id"]).update(password=password)
+                
+                return Response(serializer.data)    
+    
 
 @api_view(['GET'])
 def count_users(request, value):
