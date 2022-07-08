71c4a66c0db215a3bbebdc40a335b9d5054036eb
emadam <ldy9037@naver.com>
2022-07-08 20:13:24 +0900

    test: count view 작동 확인을 위한 test case 작성

--- user/tests/test_url.py
+++ user/tests/test_url.py
@@ -23,4 +23,18 @@ class UserTests(APITestCase):
         self.assertEqual(User.objects.get().nickname, 'hani_6_6')
         self.assertEqual(User.objects.get().phone_number, '010-5264-5565')
         self.assertTrue(PasswordHasher().verify(User.objects.get().password, "12345678"))
-        
+
+    def test_count_users(self):
+        User.objects.create(
+            email = 'ldy9037@naver.com', 
+            name = '이동열', 
+            nickname = 'hani_6_6',
+            phone_number = '010-5264-5565',
+            password = PasswordHasher().hash("12345678")
+        )
+
+        url = reverse('count-users', kwargs={'value': 'ldy9037@naver.com'})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], 1)
+        self.assertEqual(response.data['phone_number'], 0)
