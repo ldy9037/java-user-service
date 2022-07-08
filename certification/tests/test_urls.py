a6a0188197371a737e8688b2cc216f57d43ddd74
emadam <ldy9037@naver.com>
2022-07-08 13:33:58 +0900

    test: 중복확인 로직에 대한 test case 작성

--- certification/tests/test_urls.py
+++ certification/tests/test_urls.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from certification.models import Certification
+from user.models import User
 from user_service import utils 
 
 class CertificationTests(APITestCase):
@@ -12,10 +13,29 @@ class CertificationTests(APITestCase):
             'phone_number': '010-5264-5565' 
          }
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.data['message'], "인증 번호가 전송되었습니다.")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Certification.objects.count(), 1)
         self.assertEqual(Certification.objects.get().phone_number, '010-5264-5565')
         self.assertTrue(Certification.objects.get().ttl > utils.create_timestamp_ttl(0))
         self.assertIsNotNone(Certification.objects.get().number)
         self.assertIsNotNone(Certification.objects.get().created_at)
+        
+    def test_phone_number_already_exists(self):
+        url = reverse('list-insert-users')
+        data = {
+            'email': 'ldy9037@naver.com', 
+            'name': '이동열', 
+            'nickname': 'hani_6_6',
+            'phone_number': '010-5264-5565',
+            'password': "12345678"
+         }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        url = reverse('request-certification-number')
+        data = {
+            'phone_number': '010-5264-5565' 
+         }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['message'], "이미 가입된 휴대폰 번호입니다.")
