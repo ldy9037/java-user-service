398e9c796b3fbd61fdb3e1b9aa8d79a637d3c2a0
emadam <ldy9037@naver.com>
2022-07-08 14:39:54 +0900

    test: 인증번호 전송 test case 작성

---
+++ certification/tests/test_sms.py
@@ -0,0 +1,11 @@
+from email import utils
+from urllib import response
+from django.test import TestCase
+from certification import sms
+from user_service import utils 
+
+class UserTest(TestCase):
+
+    def test_publish_message(self):
+        response = sms.publish_message("01052645565", utils.create_cert_number())
+        self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], 200)
