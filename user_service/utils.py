63955828f6c5373b12d754f4d7461ea209351523
emadam <ldy9037@naver.com>
2022-07-08 11:08:37 +0900

    feat: 인증번호 생성 로직, ttl 생성 로직을 별도의 함수로 분리

---
+++ user_service/utils.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+import time
+import random
+
+def create_cert_number():
+    return str(random.randrange(1, 1000000)).rjust(6, '0')
+        
+def create_timestamp_ttl(second):
+    return int(time.mktime(datetime.now().timetuple()) * 1000) * second * 1000
