0f06896472b7614ed77db0570e6bcd797e785bcf
emadam <ldy9037@naver.com>
2022-07-08 11:57:49 +0900

    feat: path에 인증번호 요청 view 매핑 /cert

--- user_service/urls.py
+++ user_service/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from user import views
+import user.views
+import certification.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', views.list_insert_users, name='list-insert-users'),
+    path('user/', user.views.list_insert_users, name='list-insert-users'),
+    path('cert/', certification.views.request_certification_number, name='request-certification-number')
 ]
