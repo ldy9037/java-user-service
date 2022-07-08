1a84acaea006ca6c01955c0cedbc3f9d6d82d9f4
emadam <ldy9037@naver.com>
2022-07-08 10:42:07 +0900

    feat: 전화번호 인증을 위한 certification model 추가

--- certification/models.py
+++ certification/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from user_service import validators
 
-# Create your models here.
+class Certification(models.Model):
+    phone_number = models.CharField(
+        max_length=15,
+        unique=True,
+        null=False,
+        validators=[validators.validate_phone_number]
+    )
+
+    number = models.CharField(
+        max_length=6,
+        null=False
+    )
+
+    ttl = models.IntegerField(
+        null=False
+    )
+
+    created_at = models.DateTimeField(
+        null=False,
+        auto_now_add=True
+    )
