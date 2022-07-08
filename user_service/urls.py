2ca136fe9d22a50ebd48c6968f52fc475f3a0ed1
emadam <ldy9037@naver.com>
2022-07-09 08:55:46 +0900

    사용자 인증 관련 view를 path에 매핑 /api/token~

--- user_service/urls.py
+++ user_service/urls.py
@@ -3,9 +3,17 @@ from django.urls import include, path
 from rest_framework import routers
 import user.views
 import certification.views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+
     path('user/', user.views.insert_users, name='insert-users'),
     path('count/<str:value>', user.views.count_users, name='count-users'),
     path('cert/', certification.views.request_certification_number, name='request-certification-number')
