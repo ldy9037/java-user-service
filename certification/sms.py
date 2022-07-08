9b1b8cf7de3ea67a1c5e384f10ef83e4676e4409
emadam <ldy9037@naver.com>
2022-07-08 14:39:28 +0900

    feat: 인증번호 전송을 위한 publish_message 함수 추가

---
+++ certification/sms.py
@@ -0,0 +1,25 @@
+import boto3
+import logging
+
+from user_service.settings import AWS_CONFIG
+logger = logging.getLogger('pybo')
+
+def publish_message(phone_number, cert_number):
+    client = boto3.client(
+        "sns",
+        aws_access_key_id=AWS_CONFIG['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=AWS_CONFIG['AWS_SECRET_ACCESS_KEY'],
+        region_name=AWS_CONFIG['AWS_REGION']
+    )
+
+    phone_number = "+82" + phone_number
+    message = "인증 번호는 ["+cert_number+"] 입니다. \n(제한 시간: 3분)"
+
+    try:
+        response = client.publish(
+            PhoneNumber=phone_number, Message=message)
+    except boto3.ClientError:
+        logger.exception("메세지 전송에 실패하였습니다. 받는사람 : %s", phone_number)
+        raise
+    else:
+        return response
