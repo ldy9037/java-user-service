cfc43973811dbf3c5aba3ccaa108c9e2425f35a5
emadam <ldy9037@naver.com>
2022-07-08 11:57:04 +0900

    feat: 인증번호 전송 요청을 위한 view 생성

--- certification/views.py
+++ certification/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CertificationSerializer
+from user_service import utils
 
-# Create your views here.
+@api_view(['POST'])
+def request_certification_number(request):
+    if request.method == 'POST':
+        serializer = CertificationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+        
+        message = {'message': "인증 번호가 전송되었습니다."}
+
+        return Response(message, status=status.HTTP_201_CREATED)
+    
