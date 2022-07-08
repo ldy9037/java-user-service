ba74fe8dac9d655661a952ef7d4b9a93a65f49fe
emadam <ldy9037@naver.com>
2022-07-08 21:20:58 +0900

    test: test_create_user 테스트에 휴대폰 번호 인증 로직 추가

--- user/tests/test_url.py
+++ user/tests/test_url.py
@@ -3,17 +3,23 @@ from argon2 import PasswordHasher
 from rest_framework import status
 from rest_framework.test import APITestCase
 from user.models import User
+from certification.models import Certification
 
 class UserTests(APITestCase):
     def test_create_user(self):
-        
-        url = reverse('list-insert-users')
+        certification = Certification.objects.create(
+            phone_number = '010-5264-5565',
+            certified = True
+        )
+
+        url = reverse('insert-users')
         data = {
             'email': 'ldy9037@naver.com', 
             'name': '이동열', 
             'nickname': 'hani_6_6',
             'phone_number': '010-5264-5565',
-            'password': PasswordHasher().hash("12345678")
+            'plain_password': "!@#ldy12345",
+            'cert_id': certification.id
          }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -22,7 +28,7 @@ class UserTests(APITestCase):
         self.assertEqual(User.objects.get().name, '이동열')
         self.assertEqual(User.objects.get().nickname, 'hani_6_6')
         self.assertEqual(User.objects.get().phone_number, '010-5264-5565')
-        self.assertTrue(PasswordHasher().verify(User.objects.get().password, "12345678"))
+        self.assertTrue(PasswordHasher().verify(User.objects.get().password, "!@#ldy12345"))
 
     def test_count_users(self):
         User.objects.create(
