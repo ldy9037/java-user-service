69ab0b967de97f52843e214c651fde4fa1b3c07d
emadam <ldy9037@naver.com>
2022-07-08 16:50:00 +0900

    test: 불필요한 메세지 호출을 방지하기 위해 임시 주석 처리

--- certification/tests/test_sms.py
+++ certification/tests/test_sms.py
@@ -5,7 +5,9 @@ from certification import sms
 from user_service import utils 
 
 class UserTest(TestCase):
-
+    """
     def test_publish_message(self):
         response = sms.publish_message("01052645565", utils.create_cert_number())
         self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], 200)
+    """
+    
