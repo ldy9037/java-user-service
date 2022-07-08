ea6a3d20141e55489f282d8b23284444817183b7
emadam <ldy9037@naver.com>
2022-07-08 13:33:14 +0900

    refactor: 휴대폰번호 중복확인 로직 추가

--- certification/views.py
+++ certification/views.py
@@ -3,16 +3,30 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CertificationSerializer
 from user_service import utils
+from certification.models import Certification
+from user.models import User
 
 @api_view(['POST'])
 def request_certification_number(request):
     if request.method == 'POST':
         serializer = CertificationSerializer(data=request.data)
+        data = {'message': ""}
 
         if serializer.is_valid():
+            if User.objects.filter(phone_number=serializer.validated_data['phone_number']).count(): 
+                data['message'] = "이미 가입된 휴대폰 번호입니다."
+                return Response(data, status.HTTP_200_OK)
+            
             serializer.save()
-        
-        message = {'message': "인증 번호가 전송되었습니다."}
 
-        return Response(message, status=status.HTTP_201_CREATED)
+
+            cert_number = Certification.objects.filter(phone_number=serializer.data['phone_number']).latest('created_at').number
+
+
+            data['message'] = "인증 번호가 전송되었습니다. (제한 시간: 3분)"
+            return Response(data, status=status.HTTP_201_CREATED)
+        
+        
+        
+        
     
