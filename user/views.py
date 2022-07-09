2b2ac8e41e2ccad9d1e1bd519db403e7800e53c1
emadam <ldy9037@naver.com>
2022-07-09 13:37:10 +0900

    feat: 회원정보 가져오는 view 추가

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .serializers import UserSerializer
 from .models import User
@@ -36,4 +37,19 @@ def count_users(request, value):
         data['phone_number'] = count
 
         return Response(data, status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user(request, id):
+    data = {'message': ""}
+
+    if request.method == 'GET':
+        serializer = UserSerializer(User.objects.filter(id=id).get())
+
+        data['data'] = {
+            'user': serializer.data
+        }
+
+        return Response(data, status.HTTP_200_OK)
+    
 
