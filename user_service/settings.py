c23bc56d034a2132aab22bc0b179432b1790f618
emadam <ldy9037@naver.com>
2022-07-10 12:47:02 +0900

    refactor: 보안을 위해 DB 정보 분리

--- user_service/settings.py
+++ user_service/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'user_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'user_service',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': get_secret("DB_USER"),
+        'PASSWORD': get_secret("DB_PASSWORD"),
+        'HOST': get_secret("DB_HOST"),
+        'PORT': get_secret("DB_PORT")
     }
 }
 
