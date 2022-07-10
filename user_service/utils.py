af09f7a0841bc81889535f0122600b3da4a9ebd5
emadam <ldy9037@naver.com>
2022-07-10 12:46:15 +0900

    refactor: int 범위 충족을 위해 timestamp 10자리로 변경

--- user_service/utils.py
+++ user_service/utils.py
@@ -6,4 +6,4 @@ def create_cert_number():
     return str(random.randrange(1, 1000000)).rjust(6, '0')
         
 def create_timestamp_ttl(second):
-    return int(time.mktime(datetime.now().timetuple()) * 1000) * second * 1000
+    return int(time.mktime(datetime.now().timetuple())) + second
